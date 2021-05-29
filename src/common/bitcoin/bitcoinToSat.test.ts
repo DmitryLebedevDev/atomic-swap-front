806b6be1b8a8a98cc68ea6ad77a740396ceee928
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2021-05-29 11:20:39 +0800

    add test cases

--- src/common/bitcoin/bitcoinToSat.test.ts
+++ src/common/bitcoin/bitcoinToSat.test.ts
@@ -2,4 +2,6 @@ import {bitcoinToSat} from "./bitcoinToSat";
 
 test('bitcoinToSat check convert', () => {
   expect(bitcoinToSat(1)).toBe(100000000)
+  expect(bitcoinToSat(1.5)).toBe(150000000)
+  expect(bitcoinToSat(2)).toBe(200000000)
 })
